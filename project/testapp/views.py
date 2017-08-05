ff6e988120902c675af7d6198fccd1cf9d2a5564
James <james@akaintelligence.com>
2017-08-06 00:58:25 +0900

    Change template names

--- project/testapp/views.py
+++ project/testapp/views.py
@@ -8,34 +8,34 @@ def login(request):
     return TemplateResponse(request, 'login.html')
 
 def accept_invitation(request):
-    return TemplateResponse(request, 'accept-invitation.html')
+    return TemplateResponse(request, 'accept_invitation.html')
 
 def account_page_for_admin(request):
-    return TemplateResponse(request, 'account-page-for-admin.html')
+    return TemplateResponse(request, 'account_page_for_admin.html')
 
 def create_group(request):
-    return TemplateResponse(request, 'create-group.html')
+    return TemplateResponse(request, 'create_group.html')
 
 def create_group_result(request):
-    return TemplateResponse(request, 'create-group-result.html')
+    return TemplateResponse(request, 'create_group_result.html')
 
 def deposit(request):
     return TemplateResponse(request, 'deposit.html')
 
 def edit_admin_list(request):
-    return TemplateResponse(request, 'edit-admin-list.html')
+    return TemplateResponse(request, 'edit_admin_list.html')
 
 def edit_group(request):
-    return TemplateResponse(request, 'edit-group.html')
+    return TemplateResponse(request, 'edit_group.html')
 
 def edit_session(request):
-    return TemplateResponse(request, 'edit-session.html')
+    return TemplateResponse(request, 'edit_session.html')
 
 def group_dashboard(request):
-    return TemplateResponse(request, 'group-dashboard.html')
+    return TemplateResponse(request, 'group_dashboard.html')
 
 def group_setting_for_admin(request):
-    return TemplateResponse(request, 'group-setting-for-admin.html')
+    return TemplateResponse(request, 'group_setting_for_admin.html')
 
 def my_groups(request):
     return TemplateResponse(request, 'mygroups.html')
