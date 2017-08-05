59fc9fc1803daee5b29e3d31d3ec5a59b3a8a872
James <james@akaintelligence.com>
2017-08-06 00:38:39 +0900

    Create dummy views

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('studygroup.urls')),
+    url(r'^test/', include('testapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- project/testapp/views.py
+++ project/testapp/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.response import TemplateResponse
+
+
+def login(request):
+    return TemplateResponse(request, 'login.html')
+
+def accept_invitation(request):
+    return TemplateResponse(request, 'accept-invitation.html')
+
+def account_page_for_admin(request):
+    return TemplateResponse(request, 'account-page-for-admin.html')
+
+def create_group(request):
+    return TemplateResponse(request, 'create-group.html')
+
+def create_group_result(request):
+    return TemplateResponse(request, 'create-group-result.html')
+
+def deposit(request):
+    return TemplateResponse(request, 'deposit.html')
+
+def edit_admin_list(request):
+    return TemplateResponse(request, 'edit-admin-list.html')
+
+def edit_group(request):
+    return TemplateResponse(request, 'edit-group.html')
+
+def edit_session(request):
+    return TemplateResponse(request, 'edit-session.html')
+
+def group_dashboard(request):
+    return TemplateResponse(request, 'group-dashboard.html')
+
+def group_setting_for_admin(request):
+    return TemplateResponse(request, 'group-setting-for-admin.html')
+
+def my_groups(request):
+    return TemplateResponse(request, 'mygroups.html')
+
+def withdraw(request):
+    return TemplateResponse(request, 'withdraw.html')
