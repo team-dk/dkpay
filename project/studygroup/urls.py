a1b11b1e95179bc9374b007bce03265b496e3e4f
James <james@akaintelligence.com>
2017-08-06 05:24:09 +0900

    Fix misspell

--- project/studygroup/urls.py
+++ project/studygroup/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', MainView.as_view(), name='home'),
     # url(r'^$', views.login, name='login'),
     url(r'^accept-invitation/$', views.accept_invite, name='accept_invite'),
-    url(r'^create-session/$', views.create_sesion, name='create_session'),
+    url(r'^create-session/$', views.create_session, name='create_session'),
     url(r'^edit-session/?P<id>[0-9]+/$', views.edit_session, name='edit_session'),
 ]
